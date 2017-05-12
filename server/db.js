c52bcc1dc9e7440181fd72798d82df320f40fcfa
Zhao Yuan <zygg@zygg.cc>
2017-05-12 17:55:06 +0800

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ module.exports = {
     mysql: {
         host: 'zygg8007596.gotoftp2.com',
         user: 'zygg8007596',
-        password: '8007596521zygg',
+        password: '******',
         database: 'zygg8007596',
         port: '3306'
     }
